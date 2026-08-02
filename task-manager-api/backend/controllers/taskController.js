acbd7a7a1ae78c1a41fa3ffb2479b03d8f675985
Seid Star <seid@gmail.com>
2026-08-02 15:02:23 +0300

    I implmented req and res for specific taks by ID

--- task-manager-api/backend/controllers/taskController.js
+++ task-manager-api/backend/controllers/taskController.js
@@ -10,3 +10,10 @@ export function getTasks(req, res) {
   const tasks = getAllTasks(req.query);
   res.status(200).json(tasks);
 }
+export function getTask(req, res) {
+  const result = getTasksId(req.params.id);
+  if (result.notFound) {
+    return res.status(404).json(result);
+  }
+  res.status(200).json(result);
+}
