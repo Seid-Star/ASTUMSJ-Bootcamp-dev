986c84797aa24d6ee3fa468dfc8f9b1d3124fd4d
Seid Star <seid@gmail.com>
2026-08-02 14:07:43 +0300

    I implemented create new  Task

--- task-manager-api/backend/sevices/taskSevice.js
+++ task-manager-api/backend/sevices/taskSevice.js
@@ -1,7 +1,7 @@
 const { type } = require("node:os");
-const { tasks } = require("../data/tasksData");
-function getAllTasks(filters = {}) {
-  let result = tasks;
+const { tasks, getnextId } = require("../data/tasksData");
+export function getAllTasks(filters = {}) {
+  let result = [...tasks];
   if (filters.priority) {
     result = result.filter((task) => task.priority === filters.priority);
   }
@@ -12,10 +12,29 @@ function getAllTasks(filters = {}) {
   }
   return result;
 }
-function getTaskById(id) {
+export function getTaskById(id) {
   const task = tasks.find((task) => task.id === Number(id));
   if (!task) {
     throw new Error(`Task with id ${id} not found`);
   }
   return task;
+}
+export function createTask(data) {
+  const { title, priority } = data;
+  if (!title || typeof title !== "string" || title.trim() === "") {
+    return { error: "Title is required and must be non-empty string" };
+  }
+  const allowedPriority = ["low", "medium", "high"];
+  if (!allowedPriority.includes(priority)) {
+    return { error: "priority must be high,low or medium" };
+  }
+
+  const newTask = {
+    id: getnextId(),
+    title: title.trim(),
+    completed: false,
+    priority,
+  };
+  tasks.push(newTask);
+  return newTask;
 }
