6a07ad50d7a103e3272dadad0a2efbf74891a136
Seid Star <seid@gmail.com>
2026-08-02 15:55:03 +0300

    Create Express entry point and middleware setup

--- task-manager-api/backend/index.js
+++ task-manager-api/backend/index.js
@@ -2,3 +2,21 @@ import express from "express";
 import cors from "cors";
 import taskRoutes from "./routes/taskRoutes.js";
 import { PORT, APP_NAME } from "./config/env.js";
+const app = express();
+app.use(cors());
+app.use(express.json);
+app.use(".api/tasks", taskRoutes);
+app.use((req, res) => {
+  res.status(404).json({
+    error: "Route not found",
+  });
+});
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({
+    error: "Internal Server Error",
+  });
+});
+app.listen(PORT, () => {
+  console.log(`${APP_NAME} running on port ${PORT}`);
+});
