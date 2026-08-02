7ace05330cbf15e88bb0522632c76665d82774b4
Seid Star <seid@gmail.com>
2026-08-02 15:45:52 +0300

    I already Imported all thing for backend to index

--- task-manager-api/backend/config/env.js
+++ task-manager-api/backend/config/env.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
-const API_NAME = process.env.API_NAME || "Task Manager API";
+const APP_NAME = process.env.APP_NAME || "Task Manager API";
 module.exports = {
   PORT,
   API_NAME,
---
+++ task-manager-api/backend/index.js
@@ -0,0 +1,4 @@
+import express from "express";
+import cors from "cors";
+import taskRoutes from "./routes/taskRoutes.js";
+import { PORT, APP_NAME } from "./config/env.js";
