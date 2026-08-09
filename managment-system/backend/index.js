4653062a3fb3c1952e3d4dced0291d23f482d0b3
Seid Star <seid@gmail.com>
2026-08-09 18:26:48 +0300

    Add MongoDB database connection

---
+++ managment-system/backend/index.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db.js");
+const app = express();
+connectDB();
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
